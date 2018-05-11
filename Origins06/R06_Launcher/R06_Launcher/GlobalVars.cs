56ab0c92a0aeeca2d891202343f555ce4c929178
Michael Hart <bitlblox@gmail.com>
2018-05-11 13:54:30 -0700

    added new url

--- Origins06/R06_Launcher/R06_Launcher/GlobalVars.cs
+++ Origins06/R06_Launcher/R06_Launcher/GlobalVars.cs
@@ -5,6 +5,6 @@ public static class GlobalVars
 	public static string Name = "Player";
 	public static int UserID = 0;
 	public static bool ReadyToLaunch = false;
-	public static string JoinLink = "http://or06.000webhostapp.com/functions.php";
+	public static string JoinLink = "http://o06.000webhostapp.com/functions";
 	public static string ClientMD5 = "BA8B6DEE0BFD27D2D716F4122F8D6C6E";
 }
